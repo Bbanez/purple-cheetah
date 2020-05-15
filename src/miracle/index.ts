df07897b5923ab5c27487b6650afba32b0981b15
Bbanez <banezprod@gmail.com>
2020-05-15 16:11:57 +0200

    Remove os test scrypt and add index for Miracle

---
+++ src/miracle/index.ts
@@ -0,0 +1,7 @@
+export * from './cache';
+export * from './controllers';
+export * from './decorators';
+export * from './interfaces';
+export * from './interfaces';
+export * from './miracle';
+export * from './security';
